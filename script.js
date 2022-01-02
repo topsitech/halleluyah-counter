91c6d0e9b767782033387f7977d9f2d374399c14
Ajayi Temitope <topsite_edu@yahoo.com>
2022-01-02 11:49:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+		function startCount(){
+			var timeleft = 1;
+			num = document.getElementById("num").value;
+			document.getElementById("entry").style.display="none";
+			document.getElementById("Halleluyah_bottom").innerHTML= "Halleluyah!";
+			var downloadTimer = setInterval(function(){
+				if(timeleft >= num){
+						clearInterval(downloadTimer);
+						document.getElementById("countdown").innerHTML = "Halleluyah!";
+						document.getElementById("entry").style.display="block";
+						document.getElementById("Halleluyah_bottom").innerHTML= "Total of "+num;
+				} 
+				else {
+						document.getElementById("countdown").innerHTML = timeleft;
+				}
+				timeleft += 1;
+			}, 1250);
+		}
+		
